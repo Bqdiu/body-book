cf522b58320e8b1ffaabb2ce05ecfefdb3e8e789
Quang Dieu <boquangdieu2003@gmail.com>
2025-06-03 15:23:08 +0700

    backend config

---
+++ bodybook-backend/src/main/java/bqd/api/bodybook/BodyBookApplication.java
@@ -0,0 +1,14 @@
+package bqd.api.bodybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class BodyBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyBookApplication.class, args);
+	}
+
+}
---
+++ bodybook-backend/src/test/java/bqd/api/bodybook/BodybookApplicationTests.java
@@ -0,0 +1,13 @@
+package bqd.api.bodybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
